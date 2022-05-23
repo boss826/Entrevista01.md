f7a52105c925443005864d2f1ebd8aaa73ae5183
boss826 <81664887+boss826@users.noreply.github.com>
2022-05-23 01:37:49 -0500

    Add files via upload

---
+++ js/iulogica.js
@@ -0,0 +1,47 @@
+document.querySelector('#btnSaveRegister').addEventListener('click', saveRegister);
+drawRegisterTable();
+
+
+function saveRegister() {
+    var sId = document.querySelector('#txtId').value,
+        sName = document.querySelector('#txtName').value,
+        sCountry = document.querySelector('#txtCountry').value,
+        dBirthday = document.querySelector('#txtBirthday').value,
+        sEmail = document.querySelector('#txtEmail').value;
+
+    addRegisterToSystem(sId, sName, sCountry, dBirthday, sEmail);
+    drawRegisterTable();
+}
+
+function drawRegisterTable() {
+    var list = getRegisterList(),
+        tbody = document.querySelector('#registerTable tbody');
+
+
+    tbody.innerHTML = '';
+
+    for (var i = 0; i < list.length; i++) {
+        var row = tbody.insertRow(i),
+            idCell = row.insertCell(0),
+            nameCell = row.insertCell(1),
+            countryCell = row.insertCell(2),
+            birthdayCell = row.insertCell(3),
+            emailCell = row.insertCell(4),
+            selectCell = row.insertCell(5);
+
+        idCell.innerHTML = list[i].id;
+        nameCell.innerHTML = list[i].name;
+        countryCell.innerHTML = list[i].country;
+        birthdayCell.innerHTML = list[i].birthday;
+        emailCell.innerHTML = list[i].email;
+
+        var inputSelect = document.createElement('input');
+        inputSelect.type = 'radio';
+        inputSelect.value = list[i].id;
+        selectCell.appendChild(inputSelect);
+
+
+        tbody.appendChild(row);
+
+    }
+}
